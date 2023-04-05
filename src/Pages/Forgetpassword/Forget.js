a0f97b23e0ad366db90894ea17fbf77f132082da
Asfer <115686272+ASFER8384@users.noreply.github.com>
2023-04-05 15:52:51 +0530

    Add files via upload

---
+++ src/Pages/Forgetpassword/Forget.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import {Link} from 'react-router-dom'
+
+export default function Forget() {
+  const [email, setEmail] = useState("")
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+    const response = await fetch("https://full-auth.onrender.com/user/forgetpassword", {
+      method: "POST",
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify({
+        email
+      })
+    })
+    const data = await response.json()
+console.log(data)
+    if(data.status === "error"){
+      alert("user not exist")
+    }
+    if(data.status === "ok"){
+      alert("Email send sucessfully")
+    }
+  }
+
+
+
+  return (
+    <>
+    <div className="wrapper-forget">
+      <div className='forget'>
+        <form className='form' onSubmit={handleSubmit}>
+          <label style={{ textAlign: "center" }}>Forget Password</label><br></br>
+          <input
+            placeholder='Enter Your Email'
+            name='email'
+            id='email'
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+          /><br></br>
+          <button type='submit'>Forget</button><br></br><br></br>
+          <Link to="/">
+          sigin
+          </Link>
+        </form>
+      </div>
+    </div>
+    </>
+  )
+}
